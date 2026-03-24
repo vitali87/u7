286321aa0d887f1f0f2adf2ab16ce0a1c945cd19
vitali87 <eheva87@gmail.com>
2026-03-24 14:48:11 +0100

    feat: add Nix package output for installable u7

--- flake.nix
+++ flake.nix
@@ -9,45 +9,79 @@
     outputs = { self, nixpkgs, flake-utils}:
         flake-utils.lib.eachDefaultSystem (
             system:
-                let 
+                let
                     pkgs = nixpkgs.legacyPackages.${system};
-                in 
+
+                    runtimeDeps = [
+                        pkgs.curl
+                        pkgs.jq
+                        pkgs.ripgrep
+                        pkgs.fd
+                        pkgs.qsv
+
+                        pkgs.coreutils
+                        pkgs.gnused
+                        pkgs.gawk
+                        pkgs.findutils
+                        pkgs.gnugrep
+
+                        pkgs.bc
+                        pkgs.ffmpeg
+                        pkgs.imagemagick
+                        pkgs.gnutar
+                        pkgs.gzip
+                        pkgs.bzip2
+                        pkgs.xz
+                        pkgs.p7zip
+                        pkgs.unzip
+
+                        pkgs.rsync
+
+                        pkgs.openssl
+                        pkgs.yq-go
+                        pkgs.git
+                        pkgs.bash-completion
+
+                        pkgs.libwebp
+                        pkgs.gifsicle
+                        pkgs.rename
+                    ];
+                in
                 {
+                    packages.default = pkgs.stdenv.mkDerivation {
+                        pname = "u7";
+                        version = "0.1.0";
+                        src = ./.;
+
+                        buildInputs = [ pkgs.bash ] ++ runtimeDeps;
+                        nativeBuildInputs = [ pkgs.makeWrapper ];
+
+                        installPhase = ''
+                            mkdir -p $out/share/u7 $out/bin
+                            cp utility.sh $out/share/u7/utility.sh
+
+                            cat > $out/bin/u7-init <<'INITEOF'
+#!/usr/bin/env bash
+# Source this file to load u7 into your shell:
+#   source "$(u7-init)"
+echo "${placeholder "out"}/share/u7/utility.sh"
+INITEOF
+                            chmod +x $out/bin/u7-init
+
+                            # Create a wrapper script for standalone use
+                            cat > $out/bin/u7 <<'BINEOF'
+#!/usr/bin/env bash
+source "${placeholder "out"}/share/u7/utility.sh"
+u7 "$@"
+BINEOF
+                            chmod +x $out/bin/u7
+                            wrapProgram $out/bin/u7 \
+                                --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
+                        '';
+                    };
+
                     devShells.default = pkgs.mkShell {
-                        buildInputs = [
-                            pkgs.curl
-                            pkgs.jq
-                            pkgs.ripgrep
-                            pkgs.fd
-                            pkgs.qsv
-
-                            pkgs.coreutils
-                            pkgs.gnused
-                            pkgs.gawk
-                            pkgs.findutils
-                            pkgs.gnugrep
-
-                            pkgs.bc
-                            pkgs.ffmpeg
-                            pkgs.imagemagick
-                            pkgs.gnutar
-                            pkgs.gzip
-                            pkgs.bzip2
-                            pkgs.xz
-                            pkgs.p7zip
-                            pkgs.unzip
-
-                            pkgs.rsync
-
-                            pkgs.openssl
-                            pkgs.yq-go
-                            pkgs.git
-                            pkgs.bash-completion
-
-                            pkgs.libwebp
-                            pkgs.gifsicle
-                            pkgs.rename
-                        ];
+                        buildInputs = runtimeDeps;
 
                         shellHook = ''
                             source ${pkgs.bash-completion}/etc/profile.d/bash_completion.sh
